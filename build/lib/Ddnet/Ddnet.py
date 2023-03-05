f27e1a54de4ba65e2e9cf66fc367e5475cd74e69
Zichuana <2092653757@qq.com>
2023-03-05 18:44:55 +0800

    update version

--- build/lib/Ddnet/Ddnet.py
+++ build/lib/Ddnet/Ddnet.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+import os
 
 
 def info():
@@ -23,3 +25,37 @@ def draw_features(width, height, x, savename, gray=False, dpi=100):
     fig.savefig(savename, dpi=dpi)
     fig.clf()
     plt.close()
+
+
+def data_split(full_list, ratio, shuffle=True):
+    n_total = len(full_list)
+    offset = int(n_total * ratio)
+    if n_total == 0 or offset < 1:
+        return [], full_list
+    if shuffle:
+        random.shuffle(full_list)
+    sublist_1 = full_list[:offset]
+    sublist_2 = full_list[offset:]
+    return sublist_1, sublist_2
+
+
+def rename_file_tree(path):
+    # Get the list of files in the current directory
+    file_list = os.listdir(path)
+    # print(file_list)
+    global file_count, folder_count
+
+    # Traverse the file list. If the current file is not a folder, the number of files+1. If it is a folder, the number of folders+1 and then call the method of counting the number of files
+    for i in file_list:
+        path_now = path + "\\" + i
+        print(path_now)
+        if os.path.isdir(path_now) == True:
+            print(path_now, path)
+            folder_count = folder_count + 1
+            rename_file_tree(path_now)
+        else:
+            file_count = file_count + 1
+            #            print(path_now)
+            os.rename(path_now, os.path.abspath(path + '/' + 's' + str(file_count) + '.png'))
+
+    return file_count
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as fh:
 
 setuptools.setup(
     name="Ddnet",
-    version="0.0.4",
+    version="0.0.5",
     author="Zichuana",
     author_email="2092653757@qq.com",
     description="This is a test. This project is not very useful!!!",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Zichuana/Ddnet",
     packages=setuptools.find_packages(),
-    install_requires=['pandas', 'matplotlib', 'numpy', 'scipy', 'pandas_profiling', 'folium', 'seaborn', 'random'],
+    install_requires=['pandas', 'matplotlib', 'numpy', 'scipy', 'pandas_profiling', 'folium', 'seaborn', 'random', 'os'],
     # add any additional packages that needs to be installed along with SSAP package.
 
     classifiers=[
